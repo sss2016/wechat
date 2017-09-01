b27f123ae294e550a9763366d55240ffe5fa90c8
kangtongshun <kangtongshun@marchsoft.cn>
2017-09-01 11:32:08 +0800

    ''

--- resources/views/sign.blade.php
+++ resources/views/sign.blade.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <title>报名</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
     <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>

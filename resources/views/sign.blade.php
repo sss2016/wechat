2d1d7ce513cdc82453b85bf213decd95d2f4f924
kangtongshun <kangtongshun@marchsoft.cn>
2017-09-01 11:31:06 +0800

    ''

--- resources/views/sign.blade.php
+++ resources/views/sign.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>报名</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
     <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -16,7 +17,6 @@
             font-size: 40px;
         }
         .form_input{
-            font-size: 40px;
             height: 110px;
             border-radius: 20px;
         }
@@ -37,19 +37,15 @@
 <form role="form" style="margin-top: 500px;" action="{{ asset( 'sign' ) }} " method="post">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <div class="form-group">
-        <div class="sign">
-            <div class="col-sm-10">
-                <input type="text" class="form-control form_input" id="name" name="name"
-                       placeholder="请输入姓名">
-            </div>
+        <div class="col-sm-11">
+            <input type="text" class="form-control form_input sign" id="name" name="name"
+                   placeholder="请输入姓名">
         </div>
     </div>
     <div class="form-group">
-        <div class="sign">
-            <div class="col-sm-10">
-                <input type="text" class="form-control form_input" id="num" name="num"
-                       placeholder="请输入电话号码">
-            </div>
+        <div class="col-sm-11">
+            <input type="text" class="form-control form_input sign" id="num" name="num"
+                   placeholder="请输入电话号码">
         </div>
     </div>
     <button type="submit" class="btn btn-default form_submit">提交</button>
